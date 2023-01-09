9bcb50e05966da3b22388755f477842a05877ed1
ndraaditiya <andraaries1998@gmail.com>
2023-01-09 18:49:59 +0700

    edit app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <script setup>
-  import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, watch, computed } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+const inputContent = ref('')
+const inputCategory = ref(null)
+
+const todosAsc = computed(() => todos.value.sort((a, b) => {
+  return b.crreatedAt - a.crreatedAt
+}))
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || ''
+})
+
 </script>
 
 <template>
-  <div class="app">
-    Landing
-  </div>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up <input type="text" placeholder="Name Here" v-model="name" />
+      </h2>
+    </section>
+  </main>
 </template>
-
-<style scoped>
-  .app {
-    display: flex;
-    min-height: 85vh;
-    place-items: center;
-    text-transform: uppercase;
-  }
-</style>
