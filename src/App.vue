437495029c5fc14aaedf60cddc54c5ffa725aafd
ndraaditiya <andraaries1998@gmail.com>
2023-01-09 08:31:05 +0700

    commit all

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="app">
+    Landing
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+  .app {
+    display: flex;
+    min-height: 100vh;
+    place-items: center;
+    text-transform: uppercase;
+  }
+</style>
